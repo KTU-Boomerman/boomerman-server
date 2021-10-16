ee5cf1c81f87ea3596acf7a1f8e1f4dfd3748080
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-10-16 15:28:55 +0300

    Fix compile error

--- Models/Walls/Wall.cs
+++ Models/Walls/Wall.cs
@@ -6,7 +6,7 @@ namespace boomerman_server.Models.Walls
 {
     public class Wall : IDataTransferable<WallDTO>
     {
-        public Position Position { get; set; };
+        public Position Position { get; set; }
         public bool IsDestructible { get; set; }
 
         public WallDTO ToDTO()
