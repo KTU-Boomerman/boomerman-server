e26651fed222f58fcf5739374a9ceeccac08f726
Zablas <vagabaliukas@gmail.com>
2021-10-20 22:25:31 +0300

    Add position from DTO test

--- tests/DTOTests.cs
+++ tests/DTOTests.cs
@@ -1,3 +1,4 @@
+using BoomermanServer.Data;
 using BoomermanServer.Game;
 using Xunit;
 
@@ -11,6 +12,18 @@ namespace tests
             var player = new Player("A", new Position(0, 0));
             var dto = player.ToDTO();
             Assert.NotNull(dto);
+        }
+
+        [Fact]
+        public void PositionFromDTO()
+        {
+            var dto = new PositionDTO
+            {
+                X = 0,
+                Y = 0
+            };
+            var position = new Position(dto);
+            Assert.NotNull(position);
         }
     }
 }
