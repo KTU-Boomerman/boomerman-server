37cd57047a39896890375bf0be9732628e427dbf
Tomas Butkus <tomasbutkus@outlook.com>
2021-10-27 09:04:22 +0300

    edit tests cmd file

--- watch-tests.cmd
+++ watch-tests.cmd
@@ -1,1 +1,3 @@
 dotnet watch --project .\tests\ test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./lcov.info
+@REM reportgenerator -reports:"./tests/lcov.info" -targetdir:"testgenerator" -reporttypes:Html -- this generates a stats file
+@REM dotnet tool install -g dotnet-reportgenerator-globaltool
